26bbe360ef8cbc0769f7caafb1872d56e7e890ad
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-08-05 16:51:42 +0100

    adds getCities and GetCity endpoints

---
+++ Cities.Api/Controllers/CitiesController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Cities.API.Models; 
+
+namespace Cities.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController :  ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<CityDto>> GetCities()
+        {
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<CityDto> GetCity(int id)
+        {
+            var cityToReturn = CitiesDataStore.Current.Cities
+                .FirstOrDefault(city => city.Id == id);
+
+            if (cityToReturn == null)
+            {
+                return NotFound();
+            }
+            
+            return Ok(cityToReturn);
+        }
+    }
+}
