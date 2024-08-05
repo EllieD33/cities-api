83e51c5dee36f16b97400459cbea875ce1393e19
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-08-05 18:40:46 +0100

    adds points of interest endpoints

--- Cities.Api/Models/CityDto.cs
+++ Cities.Api/Models/CityDto.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace Cities.API.Models
 {
     public class CityDto
@@ -5,5 +7,16 @@ namespace Cities.API.Models
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
+
+        public int NumberOfPointsOfInterest
+        {
+            get
+            {
+                return PointsOfInterest.Count;
+            }
+        }
+
+        public ICollection<PointOfInterestDto> PointsOfInterest { get; set; }
+            = new List<PointOfInterestDto>();
     }
 }
