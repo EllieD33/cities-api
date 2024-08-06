00a764af6a02260b8d073adab10d9f13da3b09d3
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-08-06 16:06:00 +0100

    adds file download endpoint to API

---
+++ Cities.Api/Controllers/FilesController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace Cities.API.Controllers
+{
+    [Route("api/files")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+
+        private readonly FileExtensionContentTypeProvider _fileExtensionsContentTypeProvider;
+
+        public FilesController(
+            FileExtensionContentTypeProvider fileExtensionsContentTypeProvider)
+        {
+            _fileExtensionsContentTypeProvider = fileExtensionsContentTypeProvider
+                ?? throw new System.ArgumentNullException(
+                    nameof(fileExtensionsContentTypeProvider));
+        }
+
+        [HttpGet("{fileId}")]
+        public ActionResult GetFile(string fileId)
+        {
+            var pathToFile = Path.Combine(Directory.GetCurrentDirectory(), "Test_file.txt");
+            
+            if (!System.IO.File.Exists(pathToFile))
+            {
+                return NotFound();
+            }
+
+            if (!_fileExtensionsContentTypeProvider.TryGetContentType(
+                pathToFile, out var contentType))
+            {
+                contentType = "application/octet-stream";
+            }
+
+            var bytes = System.IO.File.ReadAllBytes(pathToFile);
+            return File(bytes, contentType, Path.GetFileName(pathToFile));
+        }
+    }
+}
